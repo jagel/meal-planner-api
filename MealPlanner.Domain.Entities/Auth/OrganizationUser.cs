7e6d57ddf263fa65ce9dd9482255b8580626235f
jagel <jagel8@hotmail.com>
2022-02-25 19:02:21 -0500

    Auth entities added

---
+++ MealPlanner.Domain.Entities/Auth/OrganizationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Domain.Entities.Auth
+{
+    public class OrganizationUser
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+
+        public int OrganizationId { get; set; }
+        public Organization Organization { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ MealPlanner.Domain.Entities/Auth/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Domain.Entities.Auth
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public virtual IEnumerable<OrganizationUser>? OrganizationUsers { get; set; }
+    }
+}
